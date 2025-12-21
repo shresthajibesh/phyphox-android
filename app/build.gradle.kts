aaa8fdc9f2617870b6f5f32dabecfccce66303e3
owl <dev.jibesh@gmail.com>
2025-12-21 22:58:18 +0100

    update dependency declaration to use version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,49 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "de.rwth_aachen.phyphox"
     testNamespace = "de.rwth_aachen.phyphoxTest"
-    compileSdk = 35
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "de.rwth_aachen.phyphox"
-        minSdk = 21
-        targetSdk = 35
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
 
         versionName = "1.2.0"
         //  format  WXXYYZZ, where WW is major, XX is minor, YY is patch, and ZZ is build
         versionCode = 1020009 //1.02.00-09
 
-        val locales = listOf("en", "cs", "de", "el", "es", "fr", "hi", "it", "ja", "ka", "nl", "pl", "pt", "ru", "sr", "b+sr+Latn", "tr", "vi", "zh-rCN", "zh-rTW")
-        buildConfigField("String[]", "LOCALE_ARRAY", "new String[]{\"" + locales.joinToString("\",\"") + "\"}")
+        val locales = listOf(
+            "en",
+            "cs",
+            "de",
+            "el",
+            "es",
+            "fr",
+            "hi",
+            "it",
+            "ja",
+            "ka",
+            "nl",
+            "pl",
+            "pt",
+            "ru",
+            "sr",
+            "b+sr+Latn",
+            "tr",
+            "vi",
+            "zh-rCN",
+            "zh-rTW"
+        )
+        buildConfigField(
+            "String[]",
+            "LOCALE_ARRAY",
+            "new String[]{\"" + locales.joinToString("\",\"") + "\"}"
+        )
         resourceConfigurations.addAll(locales)
 
         vectorDrawables {
@@ -76,51 +101,32 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(libs.androidx.multidex)
+    implementation(libs.google.material)
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.appcompat.resources)
+    implementation(libs.androidx.preference)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.fragment)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.viewpager)
+    implementation(libs.androidx.recyclerview.selection)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.bundles.camerax)// CameraX Bundle
+    implementation(libs.commons.io)
+    implementation(libs.zxing.android.embedded)//https://github.com/journeyapps/zxing-android-embedded/blob/master/CHANGES.md
+    implementation(libs.apache.poi)
+    implementation(libs.jlhttp)
+    implementation(libs.caverock.androidsvg)//https://bigbadaboom.github.io/androidsvg/release_notes.html
+    implementation(libs.paho.mqtt.android)
 
-    implementation("androidx.multidex:multidex:2.0.1")
+    add("androidTestScreenshotImplementation", libs.junit)
+    add("androidTestScreenshotImplementation", libs.fastlane.screengrab)
+    add("androidTestScreenshotImplementation", libs.androidx.test.rules)
+    add("androidTestScreenshotImplementation", libs.androidx.test.ext.junit)
+    add("androidTestScreenshotImplementation", libs.androidx.test.espresso.core)
 
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.annotation:annotation:1.9.1")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.appcompat:appcompat-resources:1.7.1")
-    implementation("androidx.preference:preference:1.2.1")
-    implementation("androidx.core:core:1.16.0")
-    implementation("androidx.fragment:fragment:1.8.8")
-    implementation("androidx.fragment:fragment-ktx:1.8.8")
-
-    implementation("androidx.viewpager:viewpager:1.1.0")
-    implementation("org.apache.commons:commons-io:1.3.2")
-
-    //https://github.com/journeyapps/zxing-android-embedded/blob/master/CHANGES.md
-    implementation("com.journeyapps:zxing-android-embedded:3.5.0")
-
-    implementation("org.apache.poi:poi:3.13")
-
-    implementation("net.freeutils:jlhttp:3.1")
-
-    //https://bigbadaboom.github.io/androidsvg/release_notes.html
-    implementation("com.caverock:androidsvg:1.4")
-
-    implementation("androidx.recyclerview:recyclerview-selection:1.2.0")
-    testImplementation("junit:junit:4.13.2")
-
-    //Automated screenshot generation
-//    val androidTestScreenshotImplementation by configurations.maybeCreate
-    add("androidTestScreenshotImplementation","junit:junit:4.13.2")
-    add("androidTestScreenshotImplementation","tools.fastlane:screengrab:2.1.1")
-    add("androidTestScreenshotImplementation","androidx.test:rules:1.6.1")
-    add("androidTestScreenshotImplementation","androidx.test.ext:junit:1.2.1")
-    add("androidTestScreenshotImplementation","androidx.test.espresso:espresso-core:3.6.1")
-
-    testImplementation("com.google.truth:truth:1.0.1")
-
-    implementation("com.github.hannesa2:paho.mqtt.android:4.4")
-
-    val camerax_version = "1.4.2"
-    implementation("androidx.camera:camera-core:${camerax_version}")
-    implementation("androidx.camera:camera-camera2:${camerax_version}")
-    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
-    implementation("androidx.camera:camera-view:${camerax_version}")
-
-    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    testImplementation(libs.junit)
+    testImplementation(libs.google.truth)
 }
