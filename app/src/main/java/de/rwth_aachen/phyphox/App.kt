cb75e8edf6953e24413027a870a5e23a4ca913c0
owl <dev.jibesh@gmail.com>
2025-12-25 22:19:57 +0100

    migrate app class to kotlin

--- app/src/main/java/de/rwth_aachen/phyphox/App.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/App.kt
@@ -1,12 +1,13 @@
-package de.rwth_aachen.phyphox;
+package de.rwth_aachen.phyphox
 
-import androidx.multidex.MultiDexApplication;
-
-import dagger.hilt.android.HiltAndroidApp;
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
 
 //This extension to application is only used to store measured data in memory as this may easily exceed the amount of data allowed on the transaction stack
-
 @HiltAndroidApp
-public class App extends MultiDexApplication {
-    public PhyphoxExperiment experiment = null;
+class App : MultiDexApplication() {
+
+    //Need to get rid off of this ASAP
+    @JvmField
+    var experiment: PhyphoxExperiment? = null
 }
