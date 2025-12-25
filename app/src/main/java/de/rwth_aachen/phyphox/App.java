9fad52ef9c84cde242ce1df8c3f38237da409ef1
owl <dev.jibesh@gmail.com>
2025-12-25 22:10:27 +0100

    annotate App with hiltandroidapp

--- app/src/main/java/de/rwth_aachen/phyphox/App.java
+++ app/src/main/java/de/rwth_aachen/phyphox/App.java
@@ -1,11 +1,12 @@
 package de.rwth_aachen.phyphox;
 
-import android.app.Application;
-
 import androidx.multidex.MultiDexApplication;
+
+import dagger.hilt.android.HiltAndroidApp;
 
 //This extension to application is only used to store measured data in memory as this may easily exceed the amount of data allowed on the transaction stack
 
+@HiltAndroidApp
 public class App extends MultiDexApplication {
     public PhyphoxExperiment experiment = null;
 }
