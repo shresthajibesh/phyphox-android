0de6d744a366283e5e30dc33ad400717a62bcfe2
owl <dev.jibesh@gmail.com>
2025-12-26 14:49:57 +0100

    add compose elements

---
+++ app/src/main/java/de/rwth_aachen/phyphox/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package de.rwth_aachen.phyphox.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PhyphoxPrimary = Color(0xFFFF7E22)
+val PhyphoxPrimaryWeak = Color(0xFFFFA500)
+val PhyphoxRwthPrimary = Color(0xFF00549F)
+
+val PhyphoxBlack = Color(0xFF000000)
+val PhyphoxWhite = Color(0xFFFFFFFF)
+
+// Additional colors from colors.xml if needed
+val PhyphoxRed = Color(0xFFFE005D)
+val PhyphoxGreen = Color(0xFF2BFB4C)
+val PhyphoxBlue = Color(0xFF2260A5)
+val PhyphoxYellow = Color(0xFFEDF668)
