f710d8a9e4c20be69138f251786dddf126309a53
owl <dev.jibesh@gmail.com>
2025-12-31 13:43:24 +0100

    declare default data source implementation

---
+++ app/src/main/java/de/rwth_aachen/phyphox/features/settings/data/local/DefaultLocalAppPreferencesDataSource.kt
@@ -0,0 +1,6 @@
+package de.rwth_aachen.phyphox.features.settings.data.local
+
+import de.rwth_aachen.phyphox.features.settings.domain.data.local.LocalAppPreferencesDataSource
+
+class DefaultLocalAppPreferencesDataSource: LocalAppPreferencesDataSource {
+}
