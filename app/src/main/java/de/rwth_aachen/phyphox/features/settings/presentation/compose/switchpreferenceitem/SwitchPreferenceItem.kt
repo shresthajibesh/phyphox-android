21cac9b3259c4977e2962207f2ca7e0544fc3761
owl <dev.jibesh@gmail.com>
2026-01-01 20:01:26 +0100

    update Switch Preference Item

--- app/src/main/java/de/rwth_aachen/phyphox/features/settings/presentation/compose/switchpreferenceitem/SwitchPreferenceItem.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/features/settings/presentation/compose/switchpreferenceitem/SwitchPreferenceItem.kt
@@ -3,88 +3,66 @@ package de.rwth_aachen.phyphox.features.settings.presentation.compose.switchpref
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import de.rwth_aachen.phyphox.R
+import de.rwth_aachen.phyphox.features.settings.presentation.compose.preferenceitem.PreferenceItem
+import de.rwth_aachen.phyphox.features.settings.presentation.compose.preferencesummaryitem.PreferenceSummaryItem
 import de.rwth_aachen.phyphox.features.settings.presentation.viewmodel.ResourceState
 import de.rwth_aachen.phyphox.features.settings.presentation.viewmodel.isChecked
 import de.rwth_aachen.phyphox.ui.skeleton
 import de.rwth_aachen.phyphox.ui.string.LoremIpsumStringUIModel
 import de.rwth_aachen.phyphox.ui.string.StringUIModel
-import de.rwth_aachen.phyphox.ui.string.resolve
 import de.rwth_aachen.phyphox.ui.theme.PhyphoxTheme
 
 @Composable
 fun SwitchPreferenceItem(
+    modifier: Modifier = Modifier,
     title: StringUIModel,
     summary: StringUIModel? = null,
     iconRes: Int? = null,
     checked: ResourceState<Boolean>,
     onCheckedChange: (Boolean) -> Unit,
 ) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
+
+    PreferenceItem(
+        modifier = modifier
             .clickable(checked is ResourceState.Success) {
                 onCheckedChange(checked.isChecked())
             },
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        iconRes?.let {
-            Icon(
-                painter = painterResource(id = iconRes),
-                contentDescription = null,
-                modifier = Modifier.size(24.dp),
-                tint = MaterialTheme.colorScheme.onSurfaceVariant,
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-        }
-        Column(modifier = Modifier.weight(1f)) {
-            Text(text = title.resolve(), style = MaterialTheme.typography.bodyLarge)
+        title = title,
+        iconRes = iconRes,
+        content = {
             summary?.let {
-                Text(
-                    text = summary.resolve(),
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                )
+                PreferenceSummaryItem(text = summary)
             }
-        }
-        Spacer(Modifier.size(4.dp))
-        when (checked) {
-            ResourceState.Loading -> Box(
-                modifier = Modifier
-                    .padding(top = 16.dp)
-                    .width(48.dp)
-                    .height(32.dp)
-                    .skeleton(),
-            )
-
-            is ResourceState.Success<Boolean> ->
-                Switch(
-                    checked = checked.isChecked(),
-                    onCheckedChange = onCheckedChange,
+        },
+        trailingContent = {
+            when (checked) {
+                ResourceState.Loading -> Box(
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .width(48.dp)
+                        .height(32.dp)
+                        .skeleton(),
                 )
-        }
 
-    }
+                is ResourceState.Success<Boolean> ->
+                    Switch(
+                        checked = checked.isChecked(),
+                        onCheckedChange = onCheckedChange,
+                    )
+            }
+        },
+    )
 }
 
 @Preview(
