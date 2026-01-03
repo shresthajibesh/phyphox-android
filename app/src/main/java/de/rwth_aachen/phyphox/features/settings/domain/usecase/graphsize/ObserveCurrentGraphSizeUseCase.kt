1430f3cda24407b7f7f7c5a6c98a7fd098e9b6f6
owl <dev.jibesh@gmail.com>
2026-01-03 14:56:32 +0100

    define usecases for graph size

--- app/src/main/java/de/rwth_aachen/phyphox/features/settings/domain/usecase/graphsize/ObserveCurrentGraphSizeUseCase.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/features/settings/domain/usecase/graphsize/ObserveCurrentGraphSizeUseCase.kt
@@ -1,4 +1,21 @@
 package de.rwth_aachen.phyphox.features.settings.domain.usecase.graphsize
 
-class ObserveCurrentGraphSizeUseCase {
+import de.rwth_aachen.phyphox.features.settings.domain.model.GraphConfig
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import javax.inject.Inject
+
+class ObserveCurrentGraphSizeUseCase @Inject constructor(
+
+) {
+    operator fun invoke(): Flow<GraphConfig> {
+        return flowOf(
+            GraphConfig(
+                labelSizeMultiplier = 1.0f,
+                textSizeMultiplier = 1.0f,
+                lineWidthMultiplier = 1.0f,
+                borderWidthMultiplier = 1.0f,
+            ),
+        )
+    }
 }
