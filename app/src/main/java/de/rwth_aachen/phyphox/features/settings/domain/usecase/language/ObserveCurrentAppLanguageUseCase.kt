ee9b075545c9fd3a3fae75c591ded291fde00b93
owl <dev.jibesh@gmail.com>
2026-01-03 22:19:39 +0100

    pass dummy data from app language observer

--- app/src/main/java/de/rwth_aachen/phyphox/features/settings/domain/usecase/language/ObserveCurrentAppLanguageUseCase.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/features/settings/domain/usecase/language/ObserveCurrentAppLanguageUseCase.kt
@@ -1,4 +1,12 @@
 package de.rwth_aachen.phyphox.features.settings.domain.usecase.language
 
-class ObserveCurrentAppLanguageUseCase {
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import java.util.Locale
+import javax.inject.Inject
+
+class ObserveCurrentAppLanguageUseCase @Inject constructor() {
+    operator fun invoke(): Flow<Locale> {
+        return flowOf(Locale.ENGLISH)
+    }
 }
