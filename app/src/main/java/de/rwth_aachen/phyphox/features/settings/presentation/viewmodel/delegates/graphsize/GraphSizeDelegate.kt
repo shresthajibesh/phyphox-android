6a1f2147beb96f09aad9ed6fe35a95b5cb1dd1c8
owl <dev.jibesh@gmail.com>
2026-01-03 14:24:37 +0100

    define proxy enabled delegate

--- app/src/main/java/de/rwth_aachen/phyphox/features/settings/presentation/viewmodel/delegates/graphsize/GraphSizeDelegate.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/features/settings/presentation/viewmodel/delegates/graphsize/GraphSizeDelegate.kt
@@ -2,8 +2,11 @@ package de.rwth_aachen.phyphox.features.settings.presentation.viewmodel.delegate
 
 import de.rwth_aachen.phyphox.features.settings.presentation.compose.seekbarpreferenceitem.SeekBarConfig
 import de.rwth_aachen.phyphox.utils.UiResourceState
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 
 interface GraphSizeDelegate {
     val graphSizeFlow: Flow<UiResourceState<SeekBarConfig>>
+    fun start(scope: CoroutineScope)
+    suspend fun updateGraphSize(size: Float)
 }
