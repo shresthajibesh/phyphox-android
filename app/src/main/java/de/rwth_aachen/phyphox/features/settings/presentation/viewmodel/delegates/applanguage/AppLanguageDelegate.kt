dae9821eba54a77f3ae4b231660d40f8c9a5f538
owl <dev.jibesh@gmail.com>
2026-01-03 22:19:02 +0100

    update app language delegate

--- app/src/main/java/de/rwth_aachen/phyphox/features/settings/presentation/viewmodel/delegates/applanguage/AppLanguageDelegate.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/features/settings/presentation/viewmodel/delegates/applanguage/AppLanguageDelegate.kt
@@ -1,11 +1,11 @@
 package de.rwth_aachen.phyphox.features.settings.presentation.viewmodel.delegates.applanguage
 
+import de.rwth_aachen.phyphox.ui.string.StringUIModel
 import de.rwth_aachen.phyphox.utils.UiResourceState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
-import java.util.Locale
 
 interface AppLanguageDelegate {
-    val appLanguageFlow: Flow<UiResourceState<Locale>>
+    val appLanguageFlow: Flow<UiResourceState<StringUIModel>>
     fun start(scope: CoroutineScope)
 }
--- app/src/main/java/de/rwth_aachen/phyphox/features/settings/presentation/viewmodel/delegates/applanguage/DefaultAppLanguageDelegate.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/features/settings/presentation/viewmodel/delegates/applanguage/DefaultAppLanguageDelegate.kt
@@ -3,22 +3,35 @@ package de.rwth_aachen.phyphox.features.settings.presentation.viewmodel.delegate
 import de.rwth_aachen.phyphox.features.settings.domain.usecase.language.GetSupportedLanguagesUseCase
 import de.rwth_aachen.phyphox.features.settings.domain.usecase.language.ObserveCurrentAppLanguageUseCase
 import de.rwth_aachen.phyphox.features.settings.domain.usecase.language.UpdateAppLanguageUseCase
+import de.rwth_aachen.phyphox.features.settings.presentation.viewmodel.UiBuilder
+import de.rwth_aachen.phyphox.ui.string.StringUIModel
 import de.rwth_aachen.phyphox.utils.UiResourceState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import java.util.Locale
 import javax.inject.Inject
 
-class DefaultAppLanguageDelegate @Inject constructor(
+internal class DefaultAppLanguageDelegate @Inject constructor(
     private val observeCurrentAppLanguage: ObserveCurrentAppLanguageUseCase,
     private val getSupportedLanguages: GetSupportedLanguagesUseCase,
-    private val updateAppLanguage: UpdateAppLanguageUseCase
+    private val updateAppLanguage: UpdateAppLanguageUseCase,
+    private val uiBuilder: UiBuilder,
 ) : AppLanguageDelegate {
     private val currentLanguageFlow = MutableStateFlow<UiResourceState<Locale>>(UiResourceState.Loading)
-    override val appLanguageFlow: Flow<UiResourceState<Locale>> = currentLanguageFlow.asStateFlow()
+    override val appLanguageFlow: Flow<UiResourceState<StringUIModel>> = currentLanguageFlow.map { localeResource ->
+        uiBuilder.buildLanguageUiModel(localeResource)
+    }
+
     override fun start(scope: CoroutineScope) {
-        TODO("Not yet implemented")
+        observeCurrentAppLanguage().onStart {
+            currentLanguageFlow.value = UiResourceState.Loading
+        }.onEach {
+            currentLanguageFlow.value = UiResourceState.Success(it)
+        }.launchIn(scope)
     }
 }
