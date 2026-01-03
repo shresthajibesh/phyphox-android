d1fde8397c497929512ce545049970df761a7a9c
owl <dev.jibesh@gmail.com>
2026-01-03 23:34:39 +0100

    cleanup

--- app/src/main/java/de/rwth_aachen/phyphox/features/settings/data/local/DefaultLocalAppPreferencesDataSource.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/features/settings/data/local/DefaultLocalAppPreferencesDataSource.kt
@@ -1,6 +1,14 @@
 package de.rwth_aachen.phyphox.features.settings.data.local
 
 import de.rwth_aachen.phyphox.features.settings.domain.data.local.LocalAppPreferencesDataSource
+import kotlinx.coroutines.flow.Flow
 
 class DefaultLocalAppPreferencesDataSource: LocalAppPreferencesDataSource {
+    override fun observeCurrentAccessPort(): Flow<Int?> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateAccessPort(port: Int) {
+        TODO("Not yet implemented")
+    }
 }
