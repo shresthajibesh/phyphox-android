50aa9b4fdc797681a9251402e27025f3042fae79
owl <dev.jibesh@gmail.com>
2026-01-03 13:01:34 +0100

    remove old delgates

--- app/src/main/java/de/rwth_aachen/phyphox/features/settings/presentation/viewmodel/delegates/ProximityLockDelegate.kt
+++
@@ -1,8 +0,0 @@
-package de.rwth_aachen.phyphox.features.settings.presentation.viewmodel.delegates
-
-import de.rwth_aachen.phyphox.features.settings.presentation.viewmodel.ResourceState
-import kotlinx.coroutines.flow.StateFlow
-
-interface ProximityLockDelegate {
-    val proximityLockEnabledStateFlow: StateFlow<ResourceState<Boolean>>
-}
