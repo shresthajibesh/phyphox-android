428871500fc74ceed5f12148655adc6d0fc20cc8
owl <dev.jibesh@gmail.com>
2026-01-03 13:01:19 +0100

    move UiResourceState.kt extension

--- app/src/main/java/de/rwth_aachen/phyphox/utils/UiResourceState.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/utils/UiResourceState.kt
@@ -4,3 +4,7 @@ sealed interface UiResourceState<out T> {
     data object Loading : UiResourceState<Nothing>
     data class Success<T>(val data: T) : UiResourceState<T>
 }
+
+fun UiResourceState<Boolean>.isChecked(): Boolean {
+    return this is UiResourceState.Success && this.data
+}
