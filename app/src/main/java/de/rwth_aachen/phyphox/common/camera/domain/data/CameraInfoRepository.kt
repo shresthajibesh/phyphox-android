2e9de290320e4d6814001f269c9cc578c74cb1dd
owl <dev.jibesh@gmail.com>
2026-02-15 20:55:44 +0100

    declare base data interfaces

---
+++ app/src/main/java/de/rwth_aachen/phyphox/common/camera/domain/data/CameraInfoRepository.kt
@@ -0,0 +1,11 @@
+package de.rwth_aachen.phyphox.common.camera.domain.data
+
+import de.rwth_aachen.phyphox.common.camera.domain.model.CameraInfo
+import de.rwth_aachen.phyphox.common.camera.domain.model.CameraInfoList
+
+interface CameraInfoRepository {
+    suspend fun upsertCameraInfo(cameraInfo: CameraInfo)
+    suspend fun upsertCameraInfos(cameraInfos: CameraInfoList)
+    suspend fun getCameraInfo(cameraId: String): Result<CameraInfo>
+    suspend fun getAllCameraInfos(): Result<List<CameraInfo>>
+}
