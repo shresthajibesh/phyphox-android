5de359395b5dbf67a7a5b9a58d1cd5416e358d63
owl <dev.jibesh@gmail.com>
2026-02-15 20:57:22 +0100

    declare di modules

--- app/src/main/java/de/rwth_aachen/phyphox/di/AppModule.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/di/AppModule.kt
@@ -3,8 +3,13 @@ package de.rwth_aachen.phyphox.di
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import de.rwth_aachen.phyphox.common.camera.di.CommonCameraModule
 
-@Module(includes = [])
+@Module(
+    includes = [
+        CommonCameraModule::class,
+    ],
+)
 @InstallIn(SingletonComponent::class)
 abstract class AppModule {}
 
