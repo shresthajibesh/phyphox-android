d30173065365b5ea7bad86a64e36e5f73f41e235
owl <dev.jibesh@gmail.com>
2026-02-22 15:43:57 +0100

    remove unused imports

--- app/src/main/java/de/rwth_aachen/phyphox/App.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/App.kt
@@ -1,10 +1,7 @@
 package de.rwth_aachen.phyphox
 
 import android.app.Application
-import android.hardware.camera2.CameraManager
-import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
-import de.rwth_aachen.phyphox.camera.helper.CameraHelper
 
 //This extension to application is only used to store measured data in memory as this may easily exceed the amount of data allowed on the transaction stack
 @HiltAndroidApp
