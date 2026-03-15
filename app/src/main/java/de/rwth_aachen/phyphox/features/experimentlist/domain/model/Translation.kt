69d0ffeb8bd491e83da78057011d5492a5ac95d5
owl <dev.jibesh@gmail.com>
2026-03-15 15:58:36 +0100

    update Translation.kt model

--- app/src/main/java/de/rwth_aachen/phyphox/features/experimentlist/domain/model/Translation.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/features/experimentlist/domain/model/Translation.kt
@@ -1,7 +1,7 @@
 package de.rwth_aachen.phyphox.features.experimentlist.domain.model
 
 data class Translation(
-    val locale: String,
+    val locale: String? = null,
     val title: String? = null,
     val category: String? = null,
     val description: String? = null,
