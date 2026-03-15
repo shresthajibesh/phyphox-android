336b021fe6608d33d25b9ad1fb4ca3ec6f50bfd3
owl <dev.jibesh@gmail.com>
2026-03-15 15:59:06 +0100

    make LocalizedString.kt properties nullable

--- app/src/main/java/de/rwth_aachen/phyphox/features/experimentlist/domain/model/LocalizedString.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/features/experimentlist/domain/model/LocalizedString.kt
@@ -2,6 +2,6 @@ package de.rwth_aachen.phyphox.features.experimentlist.domain.model
 
 
 data class LocalizedString(
-    val original: String,
-    val value: String,
+    val original: String?,
+    val value: String?,
 )
