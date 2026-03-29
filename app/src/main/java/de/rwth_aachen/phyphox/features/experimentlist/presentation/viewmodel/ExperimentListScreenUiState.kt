aab30b7ff247c0d5b119711ba9b7f2d6945e8198
owl <dev.jibesh@gmail.com>
2026-03-29 15:03:45 +0200

    adding display type to list

--- app/src/main/java/de/rwth_aachen/phyphox/features/experimentlist/presentation/viewmodel/ExperimentListScreenUiState.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/features/experimentlist/presentation/viewmodel/ExperimentListScreenUiState.kt
@@ -5,5 +5,13 @@ import de.rwth_aachen.phyphox.features.experimentlist.domain.model.PhyphoxExperi
 sealed interface ExperimentListScreenUiState {
     data object Loading : ExperimentListScreenUiState
     data class Error(val message: String) : ExperimentListScreenUiState
-    data class Success(val experiments: List<PhyphoxExperimentX>) : ExperimentListScreenUiState
+    data class Success(
+        val experiments: List<PhyphoxExperimentX>,
+        val displayType: DisplayType = DisplayType.List,
+    ) : ExperimentListScreenUiState
+}
+
+sealed interface DisplayType{
+    data object List : DisplayType
+    data object Grouped : DisplayType
 }
--- app/src/main/java/de/rwth_aachen/phyphox/features/experimentlist/presentation/viewmodel/ExperimentListViewModel.kt
+++ app/src/main/java/de/rwth_aachen/phyphox/features/experimentlist/presentation/viewmodel/ExperimentListViewModel.kt
@@ -4,8 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import de.rwth_aachen.phyphox.features.experimentlist.domain.LoadExperimentsFromAssetsUseCase
+import de.rwth_aachen.phyphox.features.experimentlist.domain.model.PhyphoxExperimentX
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,24 +17,46 @@ class ExperimentListViewModel @Inject constructor(
     private val loadExperimentsFromAssets: LoadExperimentsFromAssetsUseCase,
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<ExperimentListScreenUiState>(ExperimentListScreenUiState.Loading)
-    val uiState: StateFlow<ExperimentListScreenUiState> = _uiState
+    private val _experimentList = MutableStateFlow<List<PhyphoxExperimentX>>(emptyList())
+    private val _displayType = MutableStateFlow<DisplayType>(DisplayType.List)
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    private val _isLoading = MutableStateFlow(false)
+
+    val uiState = combine(
+        _experimentList,
+        _displayType,
+        _errorMessage,
+        _isLoading,
+    ) { experimentList, displayType, errorMessage, isLoading ->
+        if (isLoading) {
+            ExperimentListScreenUiState.Loading
+        } else if (errorMessage != null) {
+            ExperimentListScreenUiState.Error(errorMessage)
+        } else {
+            ExperimentListScreenUiState.Success(
+                experiments = experimentList,
+                displayType = displayType,
+            )
+        }
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.Lazily,
+        ExperimentListScreenUiState.Loading,
+    )
+
 
     init {
         loadExperimentsData()
     }
 
     private fun loadExperimentsData() = viewModelScope.launch {
+        _isLoading.value = true
         loadExperimentsFromAssets().onSuccess { result ->
-            _uiState.value = ExperimentListScreenUiState.Success(
-                result,
-            )
+            _experimentList.value = result
+            _isLoading.value = false
         }.onFailure {
-            _uiState.value = ExperimentListScreenUiState.Error(
-                it.message ?: "",
-            )
+            _errorMessage.value = it.message ?: "Unknown Error"
+            _isLoading.value = false
         }
-
-
     }
 }
